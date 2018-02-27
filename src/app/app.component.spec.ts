2dab53df4be196133c3fd32f102944a19776de45
Gabriel Landes <gabriellandes@Gabriels-MacBook-Pro-2.local>
2018-02-27 06:36:27 -0600

    add tests for Card and add, use, level methods

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,55 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'app'`, async(() => {
+  it(`should have a Card instance with inital values`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+    expect(app.card).toBeDefined();
+    expect(app.card.energy).toBe(2);
+    expect(app.card.culture).toBe(1);
+    expect(app.card.level).toBe(1);
   }));
-  it('should render title in a h1 tag', async(() => {
+  it(`should support adding energy`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.card.energy).toBe(2);
+    app.addEnergy();
+    expect(app.card.energy).toBe(3);
   }));
+  it(`should support adding culture`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.card.culture).toBe(1);
+    app.addCulture();
+    expect(app.card.culture).toBe(2);
+  }));
+  it(`should support using energy`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.card.energy).toBe(2);
+    app.useEnergy();
+    expect(app.card.energy).toBe(1);
+  }));
+  it(`should support using culture`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.card.culture).toBe(1);
+    app.useCulture();
+    expect(app.card.culture).toBe(0);
+  }));
+  it(`should support leveling up with energy`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.card.energy).toBe(2);
+    expect(app.card.level).toBe(1);
+    app.levelUp('energy');
+    expect(app.card.energy).toBe(0);
+    expect(app.card.level).toBe(2);
+  }));
+  // it('should render title in a h1 tag', async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   fixture.detectChanges();
+  //   const compiled = fixture.debugElement.nativeElement;
+  //   expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+  // }));
 });
