e1f21a207fcbc820b443dc2f8b0a3907ccf4f2ea
Gabriel Landes <gabriellandes@Gabriels-MacBook-Pro.local>
2018-03-19 18:26:29 -0500

    move app styles to app component stylesheet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Planet } from './planet';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   card: Card = new Card();
