44768480281e06b1e5afa83f1439825346d2c51b
Gabriel Landes <gabriellandes@Gabriels-MacBook-Pro.local>
2018-03-21 22:03:06 -0500

    add fullscreen button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,13 +16,37 @@ export class AppComponent {
   constructor (private planetsService: PlanetsService) {}
 
   ngOnInit () {
-    this.planetsService.getPlanets()
+    this.planetsService
+      .getPlanets()
       .subscribe(data => {
-      this.card.planets = data.map(x => new Planet(x));
+        this.card.planets = data.map(x => new Planet(x));
       })
+    ;
+    this.setupWindow();
   }
 
   toggleControlSet (set: string): void {
     this.controlSet = this.controlSet === set ? null : set;
+  }
+
+  setupWindow (): void  {
+    var col = document.getElementById('planetsCol');
+    console.log('col',col.offsetHeight, 'window', window.innerHeight);
+    col.style.height = (window.innerHeight - col.offsetHeight).toString();
+  }
+
+  toggleFullScreen () {
+    var doc = window.document;
+    var docEl = doc.documentElement;
+
+    var requestFullScreen = docEl.requestFullscreen || docEl['mozRequestFullScreen'] || docEl.webkitRequestFullScreen || docEl['msRequestFullscreen'];
+    var cancelFullScreen = doc.exitFullscreen || doc['mozCancelFullScreen'] || doc.webkitExitFullscreen || doc['msExitFullscreen'];
+
+    if(!doc.fullscreenElement && !doc['mozFullScreenElement'] && !doc.webkitFullscreenElement && !doc['msFullscreenElement']) {
+      requestFullScreen.call(docEl);
+    }
+    else {
+      cancelFullScreen.call(doc);
+    }
   }
 }
