a4ee5335d955b67c23f69682af64dba610555905
Gabriel Landes <gabe.landes@digitaldefense.com>
2019-05-11 21:22:07 -0500

    update ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,18 +35,18 @@ export class AppComponent {
   }
 
   toggleFullScreen () {
-    var doc = window.document;
-    var docEl = doc.documentElement;
+    // var doc = window.document;
+    // var docEl = doc.documentElement;
 
-    var requestFullScreen = docEl.requestFullscreen || docEl['mozRequestFullScreen'] || docEl.webkitRequestFullScreen || docEl['msRequestFullscreen'];
-    var cancelFullScreen = doc.exitFullscreen || doc['mozCancelFullScreen'] || doc.webkitExitFullscreen || doc['msExitFullscreen'];
+    // var requestFullScreen = docEl.requestFullscreen || docEl['mozRequestFullScreen'] || docEl.webkitRequestFullScreen || docEl['msRequestFullscreen'];
+    // var cancelFullScreen = doc.exitFullscreen || doc['mozCancelFullScreen'] || doc.webkitExitFullscreen || doc['msExitFullscreen'];
 
-    if(!doc.fullscreenElement && !doc['mozFullScreenElement'] && !doc.webkitFullscreenElement && !doc['msFullscreenElement']) {
-      requestFullScreen.call(docEl);
-    }
-    else {
-      cancelFullScreen.call(doc);
-    }
+    // if(!doc.fullscreenElement && !doc['mozFullScreenElement'] && !doc.webkitFullscreenElement && !doc['msFullscreenElement']) {
+    //   requestFullScreen.call(docEl);
+    // }
+    // else {
+    //   cancelFullScreen.call(doc);
+    // }
   }
 
   toggleControlSet (set: string): void {
